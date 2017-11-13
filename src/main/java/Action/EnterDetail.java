a7dfa85ac6fe36b181d6577c5c977cd85866e5f5
83chameleon <1187307315@qq.com>
2017-11-14 00:42:21 +0800

    change README.md

--- src/main/java/Action/EnterDetail.java
+++ src/main/java/Action/EnterDetail.java
@@ -1,9 +1,33 @@
 package Action;
 
+import DAO.UsersDAO;
 import Entity.UsersEntity;
+import com.opensymphony.xwork2.ActionSupport;
 
-public class EnterDetail {
+public class EnterDetail extends ActionSupport{
     private String id;
-    private String lookId;
+    private String visitId;
     private UsersEntity usersEntity;
+
+    public String execute() throws Exception{
+        UsersDAO usersDAO = new UsersDAO();
+        usersEntity = usersDAO.get(visitId);
+        return SUCCESS;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setVisitId(String visitId) {
+        this.visitId = visitId;
+    }
+
+    public UsersEntity getUsersEntity() {
+        return usersEntity;
+    }
 }
